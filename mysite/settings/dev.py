608c990eb865f302586d45630bff20d8d487f9dc
sialorama <sialorama@gmail.com>
2023-03-27 09:37:53 +0200

    Upload files for the first time

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-t5s&p++-0+v-9m-7x$t@kxa&ba_nh3i123tg)!0-m0-54f-cuo"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
